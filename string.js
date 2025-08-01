178546adea600f4aa9e292fca6a353f2dbb24c41
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-01 10:09:45 +0200

    String Practice

---
+++ string.js
@@ -0,0 +1,22 @@
+// !! Declare of a String
+
+const country = "Bangladesh";
+const division = "Dhaka";
+const district = `Dhaka`;
+const upazila = new String("Dhaka");
+
+// ?? String is imutable
+// ?? Array is mutable
+
+const str = "Today I AM going to learn JavaScript String";
+
+const upperCase = str.toUpperCase();
+const lowerCase = str.toLowerCase();
+
+// console.log(upperCase);
+// console.log(lowerCase);
+
+const trim = "   Hello World!   ";
+const trimmed = trim.trim();
+// console.log(trim);
+// console.log(trimmed);
